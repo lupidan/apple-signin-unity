ca5b03981fbac05ab989e59a198c904762cc751c
Daniel Lupiañez Casares <lupidan@gmail.com>
2019-06-08 20:38:07 +0300

    Adds success

--- Assets/AppleAuth/IOS/Interfaces/ICredentialStateResponse.cs
+++ Assets/AppleAuth/IOS/Interfaces/ICredentialStateResponse.cs
@@ -2,6 +2,7 @@ namespace AppleAuth.IOS.Interfaces
 {
     public interface ICredentialStateResponse
     {
+        bool Success { get; }
         CredentialState CredentialState { get; }
         IAppleError Error { get; }
     }
--- Assets/AppleAuth/IOS/NativeMessages/DefaultCredentialStateResponse.cs
+++ Assets/AppleAuth/IOS/NativeMessages/DefaultCredentialStateResponse.cs
@@ -6,9 +6,11 @@ namespace AppleAuth.IOS.NativeMessages
     [Serializable]
     public class DefaultCredentialStateResponse : ICredentialStateResponse
     {
+        public bool _success;
         public CredentialState _credentialState;
         public DefaultAppleError _error;
-        
+
+        public bool Success { get { return this._success; } }
         public CredentialState CredentialState { get { return this._credentialState; } }
         public IAppleError Error { get { return this._error; } }
     }
