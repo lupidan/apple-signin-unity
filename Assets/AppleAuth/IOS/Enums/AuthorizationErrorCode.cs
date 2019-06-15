75f9422f293c2d31a0a13644c3620a9984859139
Daniel Lupiañez Casares <lupidan@gmail.com>
2019-06-16 00:52:14 +0300

    Adds method to get AuthorizationErrorCode mapping ASAuthorizationError values

---
+++ Assets/AppleAuth/IOS/Enums/AuthorizationErrorCode.cs
@@ -0,0 +1,11 @@
+namespace AppleAuth.IOS.Enums
+{
+    public enum AuthorizationErrorCode
+    {
+        Unknown = 1000,
+        Canceled = 1001,
+        InvalidResponse = 1002, 
+        NotHandled = 1003,
+        Failed = 1004,
+    }
+}
