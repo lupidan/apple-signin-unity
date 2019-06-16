4d0a6145357bd9b63a297393c42fa9d8cd6ab384
Daniel Lupiañez Casares <lupidan@gmail.com>
2019-06-16 15:41:36 +0300

    Adds Unity package generation

---
+++ Assets/UnityPackageGeneration/CreateUnityPackage.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public static class CreateUnityPackage
+{
+    [MenuItem("Tools/Generate Apple Auth Unity Package")]
+    public static void GenerateUnityPackage()
+    {
+        AssetDatabase.ExportPackage("Assets/AppleAuth", "UnityAppleAuth.unitypackage", ExportPackageOptions.Recurse);
+    }
+}
