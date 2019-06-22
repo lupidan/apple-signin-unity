8acfb103e78a2307ce83c5f0d85518e68c7e9459
Daniel Lupiañez Casares <lupidan@gmail.com>
2019-06-23 00:11:16 +0300

    Adds support for Unity 2019.3

--- Assets/AppleAuthSample/Editor/SignInWithApplePostprocessor.cs
+++ Assets/AppleAuthSample/Editor/SignInWithApplePostprocessor.cs
@@ -14,9 +14,15 @@ namespace Editor
         {
             if (target != BuildTarget.iOS)
                 return;
-
+            
             var projectPath = PBXProject.GetPBXProjectPath(path);
+#if UNITY_2019_3_OR_NEWER
+            var project = new PBXProject();
+            project.ReadFromString(System.IO.File.ReadAllText(projectPath));
+            var manager = new ProjectCapabilityManager(projectPath, "Entitlements.entitlements", targetGuid: project.GetUnityMainTargetGuid());
+#else
             var manager = new ProjectCapabilityManager(projectPath, "Entitlements.entitlements", PBXProject.GetUnityTargetName());
+#endif
             manager.AddSignInWithApple();
             manager.WriteToFile();
         }
