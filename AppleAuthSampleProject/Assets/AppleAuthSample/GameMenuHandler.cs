4334d92b07668929147aa89dc02d501f87ef724e
Daniel Lupiañez Casares <lupidan@gmail.com>
2019-09-22 21:08:54 +0300

    New menu and code for the menu

---
+++ AppleAuthSampleProject/Assets/AppleAuthSample/GameMenuHandler.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using AppleAuth.IOS.Enums;
+using AppleAuth.IOS.Extensions;
+using AppleAuth.IOS.Interfaces;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class GameMenuHandler
+{
+    public GameObject Parent;
+    public Text AppleUserIdLabel;
+    public Text AppleUserCredentialLabel;
+
+    public void SetVisible(bool visible)
+    {
+        this.Parent.SetActive(visible);
+    }
+    
+    public void SetupAppleData(string appleUserId, ICredential receivedCredential)
+    {
+        this.AppleUserIdLabel.text = "Apple User ID: " + appleUserId;
+        if (receivedCredential == null)
+        {
+            this.AppleUserCredentialLabel.text = "NO CREDENTIALS RECEIVED\nProbably credential status for " + appleUserId + "was Authorized";
+            return;
+        }
+
+        var appleIdCredential = receivedCredential as IAppleIDCredential;
+        var passwordCredential = receivedCredential as IPasswordCredential;
+        if (appleIdCredential != null)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("RECEIVED APPLE ID CREDENTIAL.\nYOU CAN LOGIN/CREATE A USER WITH THIS");
+            stringBuilder.AppendLine("<b>Username:</b> " + appleIdCredential.User);
+            stringBuilder.AppendLine("<b>Real user status:</b> " + appleIdCredential.RealUserStatus.ToString());
+
+            if (appleIdCredential.State != null)
+                stringBuilder.AppendLine("<b>State:</b> " + appleIdCredential.State);
+
+            if (appleIdCredential.IdentityToken != null)
+                stringBuilder.AppendLine("<b>Identity token:</b> " + appleIdCredential.IdentityToken.Length + " bytes");
+
+            if (appleIdCredential.AuthorizationCode != null)
+                stringBuilder.AppendLine("<b>Authorization Code:</b> " + appleIdCredential.AuthorizationCode.Length + " bytes");
+
+            if (appleIdCredential.AuthorizedScopes != null)
+                stringBuilder.AppendLine("<b>Authorized Scopes:</b> " + string.Join(", ", appleIdCredential.AuthorizedScopes));
+
+            if (appleIdCredential.Email != null)
+                stringBuilder.AppendLine("<b>Email:</b> " + appleIdCredential.Email);
+
+            if (appleIdCredential.FullName != null)
+            {
+                var fullName = appleIdCredential.FullName;
+                stringBuilder.AppendLine("<b>Name:</b> " + fullName.ToLocalizedString());
+                stringBuilder.AppendLine("<b>Name (Short):</b> " + fullName.ToLocalizedString(PersonNameFormatterStyle.Short));
+                stringBuilder.AppendLine("<b>Name (Medium):</b> " + fullName.ToLocalizedString(PersonNameFormatterStyle.Medium));
+                stringBuilder.AppendLine("<b>Name (Long):</b> " + fullName.ToLocalizedString(PersonNameFormatterStyle.Long));
+                stringBuilder.AppendLine("<b>Name (Abbreviated):</b> " + fullName.ToLocalizedString(PersonNameFormatterStyle.Abbreviated));
+
+                if (appleIdCredential.FullName.PhoneticRepresentation != null)
+                {
+                    var phoneticName = appleIdCredential.FullName.PhoneticRepresentation;
+                    stringBuilder.AppendLine("<b>Phonetic name:</b> " + phoneticName.ToLocalizedString());
+                    stringBuilder.AppendLine("<b>Phonetic name (Short):</b> " + phoneticName.ToLocalizedString(PersonNameFormatterStyle.Short));
+                    stringBuilder.AppendLine("<b>Phonetic name (Medium):</b> " + phoneticName.ToLocalizedString(PersonNameFormatterStyle.Medium));
+                    stringBuilder.AppendLine("<b>Phonetic name (Long):</b> " + phoneticName.ToLocalizedString(PersonNameFormatterStyle.Long));
+                    stringBuilder.AppendLine("<b>Phonetic name (Abbreviated):</b> " + phoneticName.ToLocalizedString(PersonNameFormatterStyle.Abbreviated));
+                }
+            }
+
+            this.AppleUserCredentialLabel.text = stringBuilder.ToString();
+        }
+        else if (passwordCredential != null)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("USERNAME/PASSWORD RECEIVED (iCloud?)");
+            stringBuilder.AppendLine("<b>Username:</b> " + passwordCredential.User);
+            stringBuilder.AppendLine("<b>Password:</b> " + passwordCredential.Password);
+            this.AppleUserCredentialLabel.text = stringBuilder.ToString();
+        }
+        else
+        {
+            this.AppleUserCredentialLabel.text = "Unknown credentials for user " + receivedCredential.User;
+        }
+    }
+}
---
+++ AppleAuthSampleProject/Assets/AppleAuthSample/MainMenu.cs
@@ -0,0 +1,185 @@
+﻿using AppleAuth.IOS;
+using AppleAuth.IOS.Enums;
+using AppleAuth.IOS.Interfaces;
+using AppleAuth.IOS.NativeMessages;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private const string AppleUserIdKey = "AppleUserId";
+    
+    private IAppleAuthManager _appleAuthManager;
+    private OnDemandMessageHandlerScheduler _scheduler;
+    
+    public LoginMenuHandler LoginMenu;
+    public GameMenuHandler GameMenu;
+
+    private void Start()
+    {
+        // Creates the Scheduler to execute the pending callbacks on demand
+        this._scheduler = new OnDemandMessageHandlerScheduler();
+        // Creates a default JSON deserializer, to transform JSON Native responses to C# instances
+        var deserializer = new PayloadDeserializer();
+        // Creates an Apple Authentication manager with the scheduler and the deserializer
+        this._appleAuthManager = new AppleAuthManager(deserializer, this._scheduler);
+
+        this.SetupLoginMenu();
+    }
+
+    private void Update()
+    {
+        // Updates the scheduler to execute pending response callbacks
+        // This ensures they are executed inside Unity's Update loop
+        this._scheduler.Update();
+        
+        this.LoginMenu.UpdateLoadingMessage(Time.deltaTime);
+    }
+
+    public void SignInWithAppleButtonPressed()
+    {
+        this.SetupLoginMenuForAppleSignIn();
+        this.SignInWithApple();
+    }
+
+    public void LogoutButtonPressed()
+    {
+        this.DeleteCredentials();
+        this.SetupLoginMenu();
+    }
+
+    private void SetupLoginMenu()
+    {
+        this.LoginMenu.SetVisible(visible: true);
+        this.GameMenu.SetVisible(visible: false);
+        
+        // Check if the current platform supports Sign In With Apple
+        if (!this._appleAuthManager.IsCurrentPlatformSupported)
+        {
+            this.SetupLoginMenuForUnsupportedPlatform();
+            return;
+        }
+        
+        // If at any point we receive a credentials revoked notification, we delete the stored User ID, and go back to login
+        this._appleAuthManager.SetCredentialsRevokedCallback(result =>
+        {
+            Debug.Log("Received revoked callback " + result);
+            PlayerPrefs.DeleteKey(AppleUserIdKey);
+            this.SetupLoginMenu();
+        });
+
+        // If we have stored a Apple User Id, attempt to get the credentials for it first
+        if (PlayerPrefs.HasKey(AppleUserIdKey))
+        {
+            var storedAppleUserId = PlayerPrefs.GetString(AppleUserIdKey);
+            this.SetupLoginMenuForCheckingCredentials();
+            this.CheckCredentialStatusForUserId(storedAppleUserId);
+        }
+        // If we do not have a user ID, we just show the button to Sign In with Apple
+        else
+        {
+            this.SetupLoginMenuForSignInWithApple();
+        }
+    }
+
+    private void SetupLoginMenuForUnsupportedPlatform()
+    {
+        this.LoginMenu.SetSignInWithAppleButton(visible: false, enabled: false);
+        this.LoginMenu.SetLoadingMessage(visible: true, message: "Unsupported platform");
+    }
+    
+    private void SetupLoginMenuForSignInWithApple()
+    {
+        this.LoginMenu.SetSignInWithAppleButton(visible: true, enabled: true);
+        this.LoginMenu.SetLoadingMessage(visible: false, message: string.Empty);
+    }
+    
+    private void SetupLoginMenuForCheckingCredentials()
+    {
+        this.LoginMenu.SetSignInWithAppleButton(visible: true, enabled: false);
+        this.LoginMenu.SetLoadingMessage(visible: true, message: "Checking Apple Credentials");
+    }
+    
+    private void SetupLoginMenuForQuickLoginAttempt()
+    {
+        this.LoginMenu.SetSignInWithAppleButton(visible: true, enabled: false);
+        this.LoginMenu.SetLoadingMessage(visible: true, message: "Attempting Quick Login");
+    }
+    
+    private void SetupLoginMenuForAppleSignIn()
+    {
+        this.LoginMenu.SetSignInWithAppleButton(visible: true, enabled: false);
+        this.LoginMenu.SetLoadingMessage(visible: true, message: "Signing In with Apple");
+    }
+    
+    private void SetupGameMenu(string appleUserId, ICredential credential)
+    {
+        this.LoginMenu.SetVisible(visible: false);
+        this.GameMenu.SetVisible(visible: true);
+        this.GameMenu.SetupAppleData(appleUserId, credential);
+    }
+
+    private void CheckCredentialStatusForUserId(string appleUserId)
+    {
+        this._appleAuthManager.GetCredentialState(
+            appleUserId,
+            state =>
+            {
+                switch (state)
+                {
+                    case CredentialState.Authorized:
+                        this.SetupGameMenu(appleUserId, null);
+                        return;
+                    
+                    case CredentialState.Revoked:
+                        this.SetupLoginMenuForQuickLoginAttempt();
+                        this.AttemptQuickLogin(appleUserId);
+                        return;
+                    
+                    case CredentialState.NotFound:
+                        this.SetupLoginMenuForSignInWithApple();
+                        this.DeleteCredentials();
+                        return;
+                }
+            },
+            error =>
+            {
+                Debug.LogWarning("Error while trying to get credential state " + error.ToString());
+                this.SetupLoginMenuForSignInWithApple();
+            });
+    }
+    
+    private void AttemptQuickLogin(string appleUserId)
+    {
+        this._appleAuthManager.QuickLogin(
+            credential =>
+            {
+                this.SetupGameMenu(appleUserId, credential);
+            },
+            error =>
+            {
+                Debug.LogWarning("Quick Login Failed " + error.ToString());
+                this.SetupLoginMenuForSignInWithApple();
+            });
+    }
+    
+    private void SignInWithApple()
+    {
+        this._appleAuthManager.LoginWithAppleId(
+            LoginOptions.IncludeEmail | LoginOptions.IncludeFullName,
+            credential =>
+            {
+                PlayerPrefs.SetString(AppleUserIdKey, credential.User);
+                this.SetupGameMenu(credential.User, credential);
+            },
+            error =>
+            {
+                Debug.LogWarning("Sign in with Apple failed " + error.ToString());
+                this.SetupLoginMenuForSignInWithApple();
+            });
+    }
+
+    private void DeleteCredentials()
+    {
+        PlayerPrefs.DeleteKey(AppleUserIdKey);
+    }
+}
